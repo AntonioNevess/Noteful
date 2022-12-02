e5b4f7989841ee787b10995d1f4174e02bf7f19e
António Neves <aluno23321@ipt.pt>
2022-12-02 20:25:52 +0000

    Validacao login
    Variáveis username e password estão vazias
    
    Provavelmente erro de conversão

--- app/src/main/java/com/example/noteful/LoginActivity.kt
+++ app/src/main/java/com/example/noteful/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.example.noteful
 
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +23,38 @@ class LoginActivity : AppCompatActivity() {
         animationDrawable.setExitFadeDuration(2000)
         animationDrawable.start()
 
-        val btnRegistar = findViewById<TextView>(R.id.registarTxt)
-        btnRegistar.setOnClickListener {
+        val registar = findViewById<TextView>(R.id.registarTxt)
+        registar.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegistarActivity::class.java))
             finish()
         }
 
+        /*
+        //verificar se o username e password foram preenchidos pelo utilizador
+        val pw = findViewById<TextView>(R.id.passwordTxt)
+        val password = pw.text.toString()
+
+        val user = findViewById<TextView>(R.id.usernameTxt)
+        val username = user.text.toString()
+
+        val login = findViewById<Button>(R.id.loginBtn)
+
+
+        login.setOnClickListener{
+            if(username.isNullOrBlank() || password.isNullOrBlank()){
+                println("Username: $username")
+                println("Password:$password")
+                Toast.makeText(this, "Não inseriu nome de utilizador ou password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+        }
+
+         */
+
     }
 
-    fun transicaoLogin(view: View) {
+    fun transicaoLogin() {
         startActivity(Intent(this@LoginActivity, MainActivity::class.java))
         finish()
     }
